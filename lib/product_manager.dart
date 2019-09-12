a827a5826abf464d610ff74a45c14f810bb469c4
arifpurwandaru <ArifNazar.Purwandaru@mitrais.com>
2019-09-12 07:07:21 +0700

    changes from jangkrik

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -5,7 +5,7 @@ import './product.dart';
 class ProductManager extends StatelessWidget {
   final List<Map<String,dynamic>> products;
 
-
+//comment
   ProductManager(this.products);
   @override
   Widget build(BuildContext context) {
